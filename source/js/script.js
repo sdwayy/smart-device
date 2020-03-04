12f3d7fd8e2c16f577f5af975977b7b0cea5257e
Daniil <set2901@mail.ru>
2020-03-05 00:12:50 +0300

    Добавляет десктопный и планшетный футер проекта

--- source/js/script.js
+++ source/js/script.js
@@ -2,9 +2,18 @@
 
 var aboutCompany = document.querySelector('.about-company');
 var aboutCompanyHiddenParts = aboutCompany.getElementsByTagName('span');
+var siteNavLinksContainer = document.querySelector('.site-nav__links-container');
+var addressContainer = document.querySelector('.address__container');
+var listBtn = document.querySelector('.page-footer__list-btn');
 
-if (window.matchMedia('(max-width: 768px)').matches) {
+if (window.matchMedia('(max-width: 1023px)').matches) {
   for (var i = 0; i < aboutCompanyHiddenParts.length; i++) {
     aboutCompanyHiddenParts[i].textContent = '..';
   }
+}
+
+if (window.matchMedia('(max-width: 767px)').matches) {
+  siteNavLinksContainer.classList.add('visually-hidden');
+  addressContainer.classList.add('visually-hidden');
+  listBtn.classList.remove('visually-hidden');
 }
