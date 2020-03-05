25b376924ab99895888b8a2e9e62f7352aa64cb2
Daniil <set2901@mail.ru>
2020-03-05 22:06:40 +0300

    Добавляет комментарии с размерами изображений для ретины  и без ретины

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,6 @@ var aboutCompany = document.querySelector('.about-company');
 var aboutCompanyHiddenParts = aboutCompany.getElementsByTagName('span');
 var siteNavLinksContainer = document.querySelector('.site-nav__links-container');
 var addressContainer = document.querySelector('.address__container');
-var listBtn = document.querySelector('.page-footer__list-btn');
 
 if (window.matchMedia('(max-width: 1023px)').matches) {
   for (var i = 0; i < aboutCompanyHiddenParts.length; i++) {
@@ -15,5 +14,4 @@ if (window.matchMedia('(max-width: 1023px)').matches) {
 if (window.matchMedia('(max-width: 767px)').matches) {
   siteNavLinksContainer.classList.add('visually-hidden');
   addressContainer.classList.add('visually-hidden');
-  listBtn.classList.remove('visually-hidden');
 }
