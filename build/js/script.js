c2ad41dca88f030fa56c1361bac86c9aaf10bd0a
Daniil <set2901@mail.ru>
2020-03-06 06:51:56 +0300

    Добавляет мобильную версию футера и оверлей, исправляет баги

--- build/js/script.js
+++ build/js/script.js
@@ -1,19 +1,101 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+
 var aboutCompany = document.querySelector('.about-company');
 var aboutCompanyHiddenParts = aboutCompany.getElementsByTagName('span');
-var siteNavLinksContainer = document.querySelector('.site-nav__links-container');
-var addressContainer = document.querySelector('.address__container');
-var listBtn = document.querySelector('.page-footer__list-btn');
+var siteNav = document.querySelector('.site-nav');
+var siteNavLinksContainer = siteNav.querySelector('.site-nav__links-container');
+var address = document.querySelector('.address');
+var addressContainer = address.querySelector('.address__container');
+var listBtns = document.querySelectorAll('.page-footer_list-btn');
+var requestCallBtn = document.querySelector('.contacts__request-call-btn');
+var overlay = document.querySelector('.overlay');
+var closeOverlayBtn = overlay.querySelector('.close-btn');
+var feedbackForm = overlay.querySelector('.feedback-form');
+var feedbackFormUserName = feedbackForm.querySelector('input[name=user-name]');
+var feedbackFormUserPhone = feedbackForm.querySelector('input[name=user-tel]');
+var feedbackFormUserMessage = feedbackForm.querySelector('textarea');
+
+
+var closeOverlay = function () {
+  overlay.classList.add('visually-hidden');
+  closeOverlayBtn.removeEventListener('click', closeOverlayBtnClick);
+  document.removeEventListener('keydown', onEscPress);
+  localStorage.setItem('user-name', feedbackFormUserName.value);
+  localStorage.setItem('user-tel', feedbackFormUserPhone.value);
+  localStorage.setItem('user-message', feedbackFormUserMessage.value);
+};
+
+var onEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeOverlay();
+  }
+};
+
+var openOverlay = function () {
+  document.addEventListener('keydown', onEscPress);
+  overlay.classList.remove('visually-hidden');
+  closeOverlayBtn.addEventListener('click', closeOverlayBtnClick);
+};
+
+var onOverlayClick = function (evt) {
+  if (evt.currentTarget === overlay) {
+    closeOverlay();
+  }
+};
+
+var closeOverlayBtnClick = function () {
+  closeOverlay();
+};
+
+var onRequestCallBtnClick = function () {
+  openOverlay();
+};
+
+var onListBtnClick = function (listBtn) {
+  listBtn.classList.toggle('list-btn--active');
+
+  if (listBtn.parentElement === siteNav) {
+    siteNavLinksContainer.classList.toggle('visually-hidden');
+  }
+
+  if (listBtn.parentElement === address) {
+    addressContainer.classList.toggle('visually-hidden');
+  }
+};
+
+requestCallBtn.addEventListener('click', onRequestCallBtnClick);
+overlay.addEventListener('click', onOverlayClick);
 
 if (window.matchMedia('(max-width: 1023px)').matches) {
-  for (var i = 0; i < aboutCompanyHiddenParts.length; i++) {
-    aboutCompanyHiddenParts[i].textContent = '..';
+  requestCallBtn.removeEventListener('click', onRequestCallBtnClick);
+  overlay.removeEventListener('click', onOverlayClick);
+
+  for (
+    var hiddenPartIndex = 0;
+    hiddenPartIndex < aboutCompanyHiddenParts.length;
+    hiddenPartIndex++
+  ) {
+    aboutCompanyHiddenParts[hiddenPartIndex].textContent = '..';
   }
 }
 
 if (window.matchMedia('(max-width: 767px)').matches) {
   siteNavLinksContainer.classList.add('visually-hidden');
   addressContainer.classList.add('visually-hidden');
-  listBtn.classList.remove('visually-hidden');
+
+  for (
+    var listBtnIndex = 0;
+    listBtnIndex < listBtns.length;
+    listBtnIndex++
+  ) {
+    var currentBtn = listBtns[listBtnIndex];
+
+    currentBtn.classList.remove('list-btn--no-js');
+    currentBtn.classList.add('list-btn--js');
+    currentBtn.addEventListener('click', function (evt) {
+      onListBtnClick(evt.currentTarget);
+    });
+  }
 }
